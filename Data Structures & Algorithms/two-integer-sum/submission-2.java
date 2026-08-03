879e7f3565c0d60071b7f257482f283a78d22030
dbaca14 <144303296+dbaca14@users.noreply.github.com>
2026-08-03 13:14:43 -0500

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.java
@@ -0,0 +1,39 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         HashMap<Integer, Integer> idxTracker = new HashMap<>();
+
+//         for(int i = 0; i < nums.length; i++){
+//             idxTracker.put(nums[i], i);
+//             int diff = 0;
+//             diff = target - nums[i];
+//             if(idxTracker.containsKey(diff)){
+
+//             }
+
+//         }
+//         int diff = 0;
+//         for(int i = 0; i < nums.length; i++){
+//             diff = target - nums[i];
+//             if(idxTracker.containsKey(diff)){
+//                 int hmapI = idxTracker.get(diff);
+//                 if(i < hmapI){return new int[] {i, hmapI};}
+//                 else{return new int[] {hmapI, i};}
+//             }
+//         }
+//         return new int[] {-1,-1};
+//     }
+// }
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> d = new HashMap<>();
+        for (int i = 0;; ++i) {
+            int x = nums[i];
+            int y = target - x;
+            if (d.containsKey(y)) {
+                return new int[] {d.get(y), i};
+            }
+            d.put(x, i);
+        }
+    }
+}
